676b7ae5e3bc571260eb865e9b4c050e87314620
jovansx <jovansimic995@gmail.com>
2021-03-20 19:09:24 +0100

    Showing medicines to user is mostly done.

---
+++ Backend/PharmacyProject/src/main/java/com/team11/PharmacyProject/medicineFeatures/medicine/MedicineController.java
@@ -0,0 +1,27 @@
+package com.team11.PharmacyProject.medicineFeatures.medicine;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping(path = "api/medicine")
+public class MedicineController {
+
+    private final MedicineService medicineService;
+
+    @Autowired
+    public MedicineController(MedicineService medicineService) {
+        this.medicineService = medicineService;
+    }
+
+    @GetMapping(path = "/")
+    public List<Medicine> getMedicines() {
+        return medicineService.getAllMedicines();
+    }
+}
