4fb342348bafd9ae960099fa0f896c09b6aba242
deksa99 <deja99@live.com>
2021-04-19 13:11:41 +0200

    feature: warning modal added

--- Frontend/pharmacy-front/src/components/utilComponents/modals/WarningModal.js
+++ Frontend/pharmacy-front/src/components/utilComponents/modals/WarningModal.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Button, Modal } from 'react-bootstrap'
-import { PatchMinus } from 'react-bootstrap-icons';
+import { PatchExclamation } from 'react-bootstrap-icons';
 
-function ErrorModal(props) {
+function WarningModal(props) {
     return (
         <>
             <style type="text/css">
                 {`
-                    [variant="modal-header-error"] {
-                        background: #e85e6c;
+                    [variant="modal-header-warning"] {
+                        background: #F2CB61;
                         text-align: center;
                         margin: -5px, -5px, 0, 0;
                         padding: 10px;
@@ -16,12 +16,11 @@ function ErrorModal(props) {
                 `}
             </style>
             <Modal {...props}>
-                <Modal.Header variant='modal-header-error' className="justify-content-center" closeButton>
-                    <PatchMinus color='white' size={26}></PatchMinus>
+                <Modal.Header variant='modal-header-warning' className="justify-content-center" closeButton>
+                    <PatchExclamation color='white' size={26}></PatchExclamation>
                 </Modal.Header>
 
                 <Modal.Body className="justify-content-center">
-                    <h4>Oops!</h4>
                     <p>{props.message}</p>
                     <Button variant="outline-secondary" onClick={props.onHide} >Close</Button>
                 </Modal.Body>
@@ -30,4 +29,4 @@ function ErrorModal(props) {
     )
 }
 
-export default ErrorModal
+export default WarningModal
