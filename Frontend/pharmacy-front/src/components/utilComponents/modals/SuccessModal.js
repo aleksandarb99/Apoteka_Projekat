69603a7aea02d22251030b034346989158b40e9a
deksa99 <deja99@live.com>
2021-04-19 13:19:44 +0200

    feature: success modal added

---
+++ Frontend/pharmacy-front/src/components/utilComponents/modals/SuccessModal.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import { PatchCheck } from 'react-bootstrap-icons';
+
+function SuccessModal(props) {
+    return (
+        <>
+            <style type="text/css">
+                {`
+                    [variant="modal-header-success"] {
+                        background: #B9E883;
+                        text-align: center;
+                        margin: -5px, -5px, 0, 0;
+                        padding: 10px;
+                      }
+                `}
+            </style>
+            <Modal {...props}>
+                <Modal.Header variant='modal-header-success' className="justify-content-center" closeButton>
+                    <PatchCheck color='white' size={26}></PatchCheck>
+                </Modal.Header>
+                <Modal.Body className="justify-content-center">
+                    <p>{props.message}</p>
+                    <Button variant="outline-secondary" onClick={props.onHide} >Close</Button>
+                </Modal.Body>
+            </Modal >
+        </>
+    )
+}
+
+export default SuccessModal
