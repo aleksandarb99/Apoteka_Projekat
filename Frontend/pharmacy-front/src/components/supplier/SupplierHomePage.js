3efba0bf7ad450bb20081f144095f8a832d0fbee
deksa99 <deja99@live.com>
2021-04-26 08:45:24 +0200

    added supplier home page

--- Frontend/pharmacy-front/src/components/supplier/SupplierHomePage.js
+++ Frontend/pharmacy-front/src/components/supplier/SupplierHomePage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Col, Nav, Row, Tab } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import api from '../../app/api';
 import { getIdFromToken } from '../../app/jwtTokenUtils';
@@ -16,7 +17,36 @@ const SupplierHomePage = () => {
 
     return (
         <div>
+            <main className="unregistered__home__page">
+                <Tab.Container defaultActiveKey="first">
+                    <Row className="my-panel">
+                        <Col sm={3} md={3} lg={2} xs={12} className="sideBar">
+                            <Nav variant="pills" className="flex-column">
+                                <Nav.Item>
+                                    <Nav.Link className="my__nav__link" eventKey="first">
+                                        New requests
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link className="my__nav__link" eventKey="second">
+                                        My offers
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link className="my__nav__link" eventKey="third">
+                                        Stock
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col className="my__container" sm={9} md={9} lg={10} xs={12}>
+                            <Tab.Content>
 
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
+            </main>
             <SetPasswordModal show={!isPasswordSet} onPasswordSet={() => setIsPasswordSet(true)}></SetPasswordModal>
         </div>
     )
