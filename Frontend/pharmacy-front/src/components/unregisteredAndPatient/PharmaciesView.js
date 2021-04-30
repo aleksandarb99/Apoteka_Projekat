01f7a0e18af0ea5a24f1d89d2195f8bee6b0a300
jovansx <jovansimic995@gmail.com>
2021-04-30 12:13:39 +0200

    Redesigned front, sorter, search, filter..

--- Frontend/pharmacy-front/src/components/unregisteredAndPatient/PharmaciesView.js
+++ Frontend/pharmacy-front/src/components/unregisteredAndPatient/PharmaciesView.js
@@ -33,6 +33,9 @@ function PharmaciesView() {
   const [sorter, setSorter] = useState("none");
   const [ascDesc, setAscDesc] = useState("none");
   const [reload, setReload] = useState(false);
+  const [triggered, setTriggered] = useState("none");
+  let div = document.getElementById("my__transition__div");
+  let blocker = document.getElementById("my__blocker");
 
   useEffect(() => {
     async function fetchPharmacies() {
@@ -153,40 +156,52 @@ function PharmaciesView() {
     setReload(!reload);
   };
 
+  function hideBlocker() {
+    blocker.classList.remove("blocker");
+    div.classList.remove("my__transition__div__move");
+  }
+
   return (
     <Tab.Pane eventKey="first">
-      <Container fluid>
-        <Row className="justify-content-center mt-5 mb-0">
-          <Form onSubmit={formSearch} style={{ width: "100%" }}>
-            <Form.Group
-              as={Row}
-              className="justify-content-center align-items-center"
-            >
-              <Col lg={6}>
-                <Form.Control
-                  type="text"
-                  name="searchValue"
-                  value={fsearch}
-                  onChange={(e) => setFSearch(e.target.value)}
-                  placeholder="Enter name or place..."
-                />
-              </Col>
+      <div id="my__blocker" onClick={() => hideBlocker()}></div>
+      <div id="my__transition__div" className="my__flex">
+        <Form
+          onSubmit={formSearch}
+          style={{
+            display: triggered === "search" ? "block" : "none",
+            width: "50%",
+          }}
+        >
+          <Form.Group className="my__flex">
+            <Form.Control
+              type="text"
+              name="searchValue"
+              value={fsearch}
+              onChange={(e) => setFSearch(e.target.value)}
+              placeholder="Enter name or place..."
+            />
 
-              <Col className="justify-content-center" lg={2}>
-                <Button type="submit" className="my__search__buttons">
-                  <Search />
-                </Button>
-                <Button className="my__search__buttons" onClick={resetSearch}>
-                  Reset <Reply />
-                </Button>
-              </Col>
-            </Form.Group>
-          </Form>
-        </Row>
-        <Row>
+            <Button type="submit" className="my__search__buttons">
+              <Search />
+            </Button>
+            <Button
+              className="my__search__buttons my__flex"
+              onClick={resetSearch}
+            >
+              Reset <Reply />
+            </Button>
+          </Form.Group>
+        </Form>
+        <div
+          className="my__flex"
+          style={{ display: triggered === "sorter" ? "flex" : "none" }}
+        >
           <Form.Label style={{ marginRight: "20px" }}>Sorter: </Form.Label>
           <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
+            <Dropdown.Toggle
+              className="my__search__buttons"
+              id="dropdown-basic"
+            >
               {sorter}
             </Dropdown.Toggle>
 
@@ -222,7 +237,10 @@ function PharmaciesView() {
             </Dropdown.Menu>
           </Dropdown>
           <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
+            <Dropdown.Toggle
+              className="my__search__buttons"
+              id="dropdown-basic"
+            >
               {ascDesc}
             </Dropdown.Toggle>
 
@@ -250,55 +268,75 @@ function PharmaciesView() {
               </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-        </Row>
-        <Row>
-          <Accordion style={{ width: "100%" }}>
-            <Accordion.Toggle
-              as={Button}
-              className="my__search__buttons"
-              eventKey="0"
-            >
-              Filter
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <div>
-                <Form onSubmit={formFilter} className="my__div__filter">
-                  <Form.Group controlId="gradeSelect">
-                    <Form.Label>Grade</Form.Label>
-                    <Form.Control
-                      as="select"
-                      onChange={(event) => setFilterGrade(event.target.value)}
-                      defaultValue=""
-                    >
-                      <option value="">Not Selected</option>
-                      <option value="LOW">Low</option>
-                      <option value="MEDIUM">Medium</option>
-                      <option value="HIGH">High</option>
-                    </Form.Control>
-                  </Form.Group>
-                  <Form.Group controlId="distanceSelect">
-                    <Form.Label>Distance</Form.Label>
-                    <Form.Control
-                      as="select"
-                      onChange={(event) =>
-                        setFilterDistance(event.target.value)
-                      }
-                      defaultValue={""}
-                    >
-                      <option value={""}>Not Selected</option>
-                      <option value="5LESS">5km(Less)</option>
-                      <option value="10LESS">10km(Less)</option>
-                      <option value="10HIGHER">10km(Higher)</option>
-                    </Form.Control>
-                  </Form.Group>
-                  <Button type="submit" className="my__search__buttons">
-                    <Search />
-                  </Button>
-                </Form>
-              </div>
-            </Accordion.Collapse>
-          </Accordion>
-        </Row>
+        </div>
+        <div style={{ display: triggered === "filter" ? "block" : "none" }}>
+          <Form onSubmit={formFilter} className="my__div__filter">
+            <Form.Group controlId="gradeSelect">
+              <Form.Label>Grade</Form.Label>
+              <Form.Control
+                as="select"
+                onChange={(event) => setFilterGrade(event.target.value)}
+                defaultValue=""
+              >
+                <option value="">Not Selected</option>
+                <option value="LOW">Low</option>
+                <option value="MEDIUM">Medium</option>
+                <option value="HIGH">High</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="distanceSelect">
+              <Form.Label>Distance</Form.Label>
+              <Form.Control
+                as="select"
+                onChange={(event) => setFilterDistance(event.target.value)}
+                defaultValue={""}
+              >
+                <option value={""}>Not Selected</option>
+                <option value="5LESS">5km(Less)</option>
+                <option value="10LESS">10km(Less)</option>
+                <option value="10HIGHER">10km(Higher)</option>
+              </Form.Control>
+            </Form.Group>
+            <Button type="submit" className="my__search__buttons">
+              <Search />
+            </Button>
+          </Form>
+        </div>
+      </div>
+
+      <div className="my__flex" style={{ paddingTop: "30px" }}>
+        <Button
+          className="my__search__buttons"
+          onClick={() => {
+            div.classList.add("my__transition__div__move");
+            blocker.classList.add("blocker");
+            setTriggered("search");
+          }}
+        >
+          Search
+        </Button>
+        <Button
+          className="my__search__buttons"
+          onClick={() => {
+            div.classList.add("my__transition__div__move");
+            blocker.classList.add("blocker");
+            setTriggered("sorter");
+          }}
+        >
+          Sorter
+        </Button>
+        <Button
+          className="my__search__buttons"
+          onClick={() => {
+            div.classList.add("my__transition__div__move");
+            blocker.classList.add("blocker");
+            setTriggered("filter");
+          }}
+        >
+          Filter
+        </Button>
+      </div>
+      <Container fluid>
         <Row>
           {showedPharmacies.length === 0 && (
             <Row className="justify-content-center m-3 align-items-center">
