c0828a62f244e80650f613352d6c77bab36c70ae
deksa99 <deja99@live.com>
2021-05-03 20:00:09 +0200

    added list to response to a complaint

--- Frontend/pharmacy-front/src/components/complaintResponses/NewComplaintsList.js
+++ Frontend/pharmacy-front/src/components/complaintResponses/NewComplaintsList.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ComplaintResponse from './ComplaintResponse'
+import api from '../../app/api'
 
 const NewComplaintsList = () => {
+    const [complaints, setComplaints] = useState([])
+
+    useEffect(() => {
+        async function fetchComplaints() {
+            api.get(`http://localhost:8080/api/complaints/`)
+                .then((res) => {
+                    setComplaints(res.data)
+                })
+        }
+        fetchComplaints();
+    }, [])
+
     return (
         <div>
-            <p>List</p>
+            {complaints && complaints.map((c) => {
+                return <ComplaintResponse key={c.id} complaint={c}></ComplaintResponse>
+            })}
         </div>
     )
 }
