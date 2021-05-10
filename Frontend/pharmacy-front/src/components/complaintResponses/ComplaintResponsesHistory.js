613cca9365488d1b6bb1b74920b753ca57ed0820
deksa99 <deja99@live.com>
2021-05-10 10:20:53 +0200

    complaint history (admin)

--- Frontend/pharmacy-front/src/components/complaintResponses/ComplaintResponsesHistory.js
+++ Frontend/pharmacy-front/src/components/complaintResponses/ComplaintResponsesHistory.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../app/api'
+import { getIdFromToken } from '../../app/jwtTokenUtils'
+import ComplaintResponseDetails from './ComplaintResponseDetails'
 
 const ComplaintResponsesHistory = () => {
+    const [complaintResponses, setComplaintResponses] = useState([])
+
+    useEffect(() => {
+        async function fetchComplaints() {
+            api.get(`http://localhost:8080/api/complaint-responses/admin/${getIdFromToken()}`)
+                .then((res) => {
+                    setComplaintResponses(res.data)
+                })
+        }
+        fetchComplaints();
+    }, [])
+
     return (
         <div>
-            <p>HISTORY</p>
+            {complaintResponses && complaintResponses.map((cr) => {
+                return <ComplaintResponseDetails key={cr.id} complaintResponse={cr}></ComplaintResponseDetails>
+            })}
         </div>
     )
 }
