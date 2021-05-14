0939a02a9250a0b58f3e4e2881f9727b70a1cde8
deksa99 <deja99@live.com>
2021-05-14 14:07:28 +0200

    added endpoint to dispense medicine in selected pharmacy

--- Backend/PharmacyProject/src/main/java/com/team11/PharmacyProject/eRecipe/ERecipeController.java
+++ Backend/PharmacyProject/src/main/java/com/team11/PharmacyProject/eRecipe/ERecipeController.java
@@ -6,11 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("api/e-recipes")
@@ -24,6 +23,16 @@ public class ERecipeController {
 
         ERecipeDTO eRecipeDTO = eRecipeService.getERecipe(file);
         return new ResponseEntity<>(eRecipeDTO, HttpStatus.OK);
+    }
+
+    @PostMapping(value="/dispense-medicine/{pharmacyId}")
+    public ResponseEntity<?> dispenseMedicine(@PathVariable("pharmacyId") long pharmacyId, @RequestBody @Valid ERecipeDTO eRecipeDTO) {
+        boolean isDispensed = eRecipeService.dispenseMedicine(pharmacyId, eRecipeDTO);
+        if (isDispensed) {
+            return new ResponseEntity<>("Medicine successfully dispensed", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Error. Medicine not dispensed", HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
--- Backend/PharmacyProject/src/main/java/com/team11/PharmacyProject/eRecipe/ERecipeService.java
+++ Backend/PharmacyProject/src/main/java/com/team11/PharmacyProject/eRecipe/ERecipeService.java
@@ -5,4 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ERecipeService {
     ERecipeDTO getERecipe(MultipartFile file);
+
+    boolean dispenseMedicine(long pharmacyId, ERecipeDTO eRecipeDTO);
 }
