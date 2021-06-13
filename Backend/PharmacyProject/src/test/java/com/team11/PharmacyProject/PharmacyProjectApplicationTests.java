1457e89ab54bf90890e2f1301a1fba5002632c6b
Aleksandar Buljevic <abuljevic8@gmail.com>
2021-06-13 14:30:23 +0200

    added line in travis.yml

--- Backend/PharmacyProject/src/test/java/com/team11/PharmacyProject/PharmacyProjectApplicationTests.java
+++ Backend/PharmacyProject/src/test/java/com/team11/PharmacyProject/PharmacyProjectApplicationTests.java
@@ -3,10 +3,11 @@ package com.team11.PharmacyProject;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration
 class PharmacyProjectApplicationTests {
 
 	@Test
